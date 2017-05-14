4f19e57bea59ace5dab7eb682e4a792ec3b16936
Zach <zachcopland@Zachs-MacBook-Pro.local>
2017-05-14 16:12:26 -0400

    added chart option

--- event-db/listEvents.php
+++ event-db/listEvents.php
@@ -2,13 +2,27 @@
 include '../db/dbh.php';
 
 $org = $_POST['org'];
+$org_orig = $org;
 $org = ucwords($org);
 $org = str_replace(' ', '', $org);
 $school = $_POST['school'];
 $school = ucwords($school);
 $school = str_replace(' ', '', $school);
+$count = 0;
+$labels = '';
+$data = '';
 
-$opening = "<div id=\"events-list\" class=\"table-responsive\"><table class='table table-hover'><tr><th>Event title</th><th>Date</th><th>Guests</th></tr>";
+$opening = <<<TEXT
+<div class="btn-group">
+    <button type="button" id="chart-btn" class="btn btn-primary" onclick="chartTable('chart')">Chart</button>
+    <button type="button" id="table-btn" class="btn btn-primary" disabled="true" onclick="chartTable('table')">Table</button>
+</div>
+<input id='ORG' type='hidden' value='{$org_orig}' />
+<div id='chartDiv' style="width:400px; height:400px;">
+    <canvas id="chart" width="400px" height="400px"></canvas>
+</div>
+<div id="events-list" class="table-responsive"><table class='table table-hover'><tr><th>Event title</th><th>Date</th><th>Guests</th></tr>
+TEXT;
 $body = '';
 $closing = "</tr></table></div>";
 
@@ -16,6 +30,14 @@ $closing = "</tr></table></div>";
 $sql = "SELECT * FROM `partyList_{$org}_{$school}`;";
 $result = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
+    if ($count == 0) {
+        $labels .= $row['eventTitle'];
+        $data .= $row['count'];
+    } else {
+        $labels .= ",{$row['eventTitle']}";
+        $data .= ",{$row['count']}";
+    }
+    $count++;
     $body .= <<<TEXT
 <tr>
   <td>{$row['eventTitle']}</td>
@@ -24,6 +46,11 @@ while ($row = mysqli_fetch_assoc($result)) {
 </tr>
 TEXT;
 }
+
+$closing .= <<<TEXT
+<input id='labels' type='hidden' value='{$labels}' />
+<input id='data' type='hidden' value='{$data}' />
+TEXT;
 
 if (!empty($body)) {
     $body = $opening . $body . $closing;
